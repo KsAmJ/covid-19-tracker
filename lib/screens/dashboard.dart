86114ce5e34bd344da4cc0616a2c30ac7d8ca49b
Khaled Ahmed <ksamj202@gmail.com>
2020-04-27 17:04:05 +0400

    stream 2

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,7 +1,8 @@
-import 'package:country_code_picker/country_code_picker.dart';
 import 'package:covid19_tracker/model/covid19_dashboard.dart';
 import 'package:covid19_tracker/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:country_pickers/country_pickers.dart';
+import 'package:country_pickers/country.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key key}) : super(key: key);
@@ -22,24 +23,64 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
+        title: Text('COVID-19 Dashboard'),
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ListTile(
-          leading: CountryCodePicker(
-            onChanged: print,
-            initialSelection: data.countries[index].countryCode,
-            showCountryOnly: false,
-            showFlag: true,
-          ),
-          title: Text('${data.countries[index].country}'),
-          trailing: Text('${data.countries[index].confirmed}'),
-        );
-      }),
+      body: data == null
+          ? Center(child: CircularProgressIndicator())
+          : RefreshIndicator(
+              onRefresh: getData,
+              child: ListView.builder(
+                  itemCount: data.countries.length,
+                  itemBuilder: (context, index) {
+                    return ExpansionTile(
+                      leading: data.countries[index].countryCode.length == 2
+                          ? CountryPickerUtils.getDefaultFlagImage(Country(
+                              isoCode: data.countries[index].countryCode))
+                          : Text(''),
+                      title: Text('${data.countries[index].country}'),
+                      trailing: Text('${data.countries[index].confirmed}'),
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Rank: ${index + 1}'),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                        'Confirmed: ${data.countries[index].confirmed}'),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                        'Recovered: ${data.countries[index].recovered}',
+                                        style: TextStyle(color: Colors.green)),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                        'Deaths: ${data.countries[index].deaths}',
+                                        style: TextStyle(color: Colors.red)),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                  ]),
+                            ],
+                          ),
+                        )
+                      ],
+                    );
+                  }),
+            ),
     );
   }
 
-  getData() async {
+  Future<void> getData() async {
     Networking network = Networking();
     Covid19Dashboard result = await network.getDashboardData();
     setState(() {
